6ecf49ff91989d36dfad0f945cc1fd76d693da9f
Javier Alberto <alu0100836400@ull.edu.es>
2019-02-21 16:58:20 +0000

    Travis 2

--- basesDeDatos/gulpfile.js
+++ basesDeDatos/gulpfile.js
@@ -2,4 +2,4 @@ let gulp = require("gulp");
 let shell = require("gulp-shell");
 const mocha = require("gulp-mocha");
 
-gulp.task('hello', shell.task("cd basesDeDatos && npm run test:debug"));
+gulp.task('hello', shell.task("npm run test:debug"));
